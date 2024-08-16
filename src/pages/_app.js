b2fe208f522d3fe69c8dc7f2dca47c911a01c109
rebecarivas <rebecarivas@gmail.com>
2024-08-16 16:37:50 -0300

    adicona o `_app.js`

---
+++ src/pages/_app.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Global from "../../styles/global.js";
+import Layout from "../Components/Layout/index.js";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Global />
+      <Head>
+        <title>Marcio Silva</title>
+        <meta
+          name="description"
+          content="Página pessoal do baterista Marcio Silva"
+        />
+
+        <link
+          rel="shortcut icon"
+          href="/assets/MSicone1.png"
+          type="image/png"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
+export default MyApp;
