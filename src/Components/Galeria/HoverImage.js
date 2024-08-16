a14db7d9a84f4849dbbd50ea4ea73a4e83d299a7
rebecarivas <rebecarivas@gmail.com>
2024-08-16 16:41:07 -0300

    adiciona página galeria

---
+++ src/Components/Galeria/HoverImage.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+function HoverImage({ src, alt, text }) {
+  return (
+    <HoverContainer>
+      <ImageWrapper>
+        <Image src={src} alt={alt} responsive />
+      </ImageWrapper>
+      <HoverText>
+        {text.map((line, index) => (
+          <p key={index}>{line}</p>
+        ))}
+      </HoverText>
+    </HoverContainer>
+  );
+}
+
+export default HoverImage;
+
+const HoverText = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.5);
+  color: var(--gray);
+  font-weight: bold;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  text-align: center;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  p {
+    margin: 0.2em 0;
+  }
+`;
+
+const HoverContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  margin-bottom: 30px;
+  &:hover ${HoverText} {
+    opacity: 1;
+  }
+`;
+const ImageWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 500px;
+  img {
+    width: 100%;
+    height: auto;
+    border: 6px solid var(--gray);
+  }
+  @media (max-width: 768px) {
+    width: 300px;
+  }
+`;
---
+++ src/pages/galeria.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+import HoverImage from "../Components/Galeria/HoverImage";
+import MarcioSanta from "../../public/assets/MarcioSanta.jpg";
+import Circular from "../../public/assets/circular.jpg";
+import SaracaColombia from "../../public/assets/SaracotiaColombia2.jpg";
+import Fimdefeira from "../../public/assets/fimde.jpg";
+import Marciocolombia from "../../public/assets/Marciocolombia.jpg";
+import Marciovert from "../../public/assets/marciovert.jpg";
+import Circular2 from "../../public/assets/circular2.jpg";
+import Marcio2 from "../../public/assets/marcio2.jpg";
+import Vasco from "../../public/assets/vasco.jpg";
+import Brasil from "../../public/assets/brasil.jpg";
+import { useTranslation } from "react-i18next";
+
+function Galeria() {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <>
+      <Wrapper>
+        <div className="container">
+          <h1>{t("Galeria")}</h1>
+          <Content>
+            <HoverImage
+              src={MarcioSanta}
+              alt={
+                "Marcio Silva na bateria, Teatro Santa Izabel, Recife em 2015"
+              }
+              text={["Saracotia", "Teatro Santa Izabel - Recife", "2015"]}
+            />
+            <HoverImage
+              src={Circular}
+              alt={"Grupo Circular Caxangá no palco"}
+              text={["Circular Caxangá", "Teatro Paiol - Curitiba", "2013"]}
+            />
+            <HoverImage
+              src={SaracaColombia}
+              alt={"Saracotia tocando na Colômbia"}
+              text={["Saracotia", "Colômbia", "2014"]}
+            />
+            <HoverImage
+              src={Fimdefeira}
+              alt={"Grupo Fim de Feira"}
+              text={["Fim de Feira", "Recife", "2014"]}
+            />
+            <HoverImage
+              src={Marciocolombia}
+              alt={"Marcio Silva tocando bateria na Colômbia"}
+              text={["Saracotia", "Colômbia", "2014"]}
+            />
+            <HoverImage
+              src={Marciovert}
+              alt={"Marcio Silva tocando bateria"}
+              text={["Saracotia", "Teatro Luiz Mendonça - Recife", "2012"]}
+            />
+            <HoverImage
+              src={Circular2}
+              alt={"Grupo Circular Caxangá"}
+              text={[
+                "Circular Caxangá",
+                "Faculdade Souza Lima - São Paulo",
+                "2013",
+              ]}
+            />
+            <HoverImage
+              src={Marcio2}
+              alt={"Marcio Silva tocando bateria"}
+              text={["Saracotia", "Teatro Santa Izabel - Recife", "2015"]}
+            />
+            <HoverImage
+              src={Vasco}
+              alt={"Marcio tocando bateria em Portugal"}
+              text={["Aveiro - Portugal", "2015"]}
+            />
+            <HoverImage
+              src={Brasil}
+              alt={""}
+              text={[
+                "Circular Caxangá",
+                "Faculdade Souza Lima - São Paulo",
+                "2013",
+              ]}
+            />
+          </Content>
+        </div>
+      </Wrapper>
+    </>
+  );
+}
+export default Galeria;
+
+const Wrapper = styled.main`
+  padding-top: 30px;
+  padding-bottom: 30px;
+  background-color: var(--lightblack);
+`;
+const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  @media (max-width: 1200px) {
+    justify-content: center;
+  }
+`;
